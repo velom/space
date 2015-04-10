cdd1600931f0e0c0cdf5b55a38906803ed5f09df
velom <velom@yandex-team.ru>
2015-04-10 10:48:55 +0300

    add space.py + README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# space
+Интерфейс работы с экземплярами кораблей и планет.
+
+Пример:
+```python
+>>> from space import *
+>>> v = Vessel('velom', [0,0], 1000)
+>>> e = Planet('Earth', [100,100], 5000)
+>>> m = Planet('Mars', [1000,1000], 000)
+>>> m.report()
+Mars. Местоположение: [1000, 1000]. Товаров нет.
+>>> e.report()
+Earth. Местоположение: [100, 100]. Груз: 5000т.
+>>> v.flyTo([100,100])
+>>> v.report()
+velom. Местоположение: [100, 100]. Товаров нет.
+>>> e.loadCargoTo(v, 1200)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "space.py", line 49, in loadCargoTo
+    raise Exception("Not such free space at %s" % vessel.name)
+Exception: Not such free space at velom
+>>> e.loadCargoTo(v, 1000)
+>>> v.report()
+velom. Местоположение: [100, 100]. Груз: 1000т.
+>>> e.report()
+Earth. Местоположение: [100, 100]. Груз: 4000т.
+>>> v.flyTo(m)
+>>> m.unloadCargoFrom(v, 1200)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "space.py", line 58, in unloadCargoFrom
+    raise Exception("Not such available cargo at %s" % vessel.name)
+Exception: Not such available cargo at velom
+>>> m.unloadCargoFrom(v, 1000)
+>>> m.report()
+Mars. Местоположение: [1000, 1000]. Груз: 1000т.
+>>> e.report()
+Earth. Местоположение: [100, 100]. Груз: 4000т.
+>>> v.report()
+velom. Местоположение: Mars. Товаров нет.
+```
---
+++ space.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+class Vessel(object):
+    def __init__(self, name, position, capacity):
+        self.name = str(name)
+        self.position = list(position)
+        self.capacity = int(capacity)
+        self.occupied = 0
+    def report(self):
+        if not self.occupied:
+            _occupied = "Товаров нет"
+        else:
+            _occupied = "Груз: %dт" % self.occupied
+        print "%s. Местоположение: %s. %s." % (self.name, self.position, _occupied)
+    def getFreeSpace(self):
+        return self.capacity - self.occupied
+    def getOccupiedSpace(self):
+        return self.occupied
+    def flyTo(self, newPosition):
+        if isinstance(newPosition, Planet):
+            self.position = newPosition.name
+        else:
+            self.position = list(newPosition)
+
+
+class Planet(object):
+    def __init__(self, name, position, availableAmountOfCargo):
+        self.name = str(name)
+        self.position = list(position)
+        self.availableAmountOfCargo = int(availableAmountOfCargo)
+    def report(self):
+        if not self.availableAmountOfCargo:
+            _occupied = "Товаров нет"
+        else:
+            _occupied = "Груз: %dт" % self.availableAmountOfCargo
+        print "%s. Местоположение: %s. %s." % (self.name, self.position, _occupied)
+    def getAvailableAmountOfCargo(self):
+        return self.availableAmountOfCargo
+    def loadCargoTo(self, vessel, cargoWeight):
+        if vessel.position == self.name or vessel.position == self.position:
+            if vessel.getFreeSpace() >= cargoWeight:
+                if self.getAvailableAmountOfCargo() >= cargoWeight:
+                    vessel.occupied += cargoWeight
+                    self.availableAmountOfCargo -= cargoWeight
+                else:
+                    raise Exception("Not such available cargo at %s" % self.name)
+            else:
+                raise Exception("Not such free space at %s" % vessel.name)
+        else:
+            raise Exception("Vessel %s is not on the planet %s" % (vessel.name, self.name))
+    def unloadCargoFrom(self, vessel, cargoWeight):
+        if vessel.position == self.name or vessel.position == self.position:
+            if vessel.getOccupiedSpace() >= cargoWeight:
+                vessel.occupied -= cargoWeight
+                self.availableAmountOfCargo += cargoWeight
+            else:
+                raise Exception("Not such available cargo at %s" % vessel.name)
+        else:
+            raise Exception("Vessel %s is not on the planet %s" % (vessel.name, self.name))
